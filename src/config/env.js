187544c10d3a70d6101e6454bc5dd16d31bc83e9
LAPTOP-I427NBL1\Suhan <614234024@parichat.skru.ac.th>
2021-06-19 03:23:12 +0700

    :wrench: add default values of config

--- src/config/env.js
+++ src/config/env.js
@@ -3,9 +3,9 @@ const config = {
   serverport: process.env.SV_PORT || 3000,
   serverHost: process.env.SV_HOST || 'localhost',
   databaseHost: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME || 'booking',
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASS || '',
 }
 
 module.exports = config
